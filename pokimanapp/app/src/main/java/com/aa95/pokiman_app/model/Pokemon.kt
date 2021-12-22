f94b71ef705cfaffb59d910d3f7949fe24626cbe
Brandon-Reyna <brandonreynaruiz@gmail.com>
2021-12-21 19:24:05 -0600

    changed properties for pokemon

--- pokimanapp/app/src/main/java/com/aa95/pokiman_app/model/Pokemon.kt
+++ pokimanapp/app/src/main/java/com/aa95/pokiman_app/model/Pokemon.kt
@@ -1,3 +1,3 @@
 package com.aa95.pokiman_app.model
 
-data class Pokemon(val name: String, var hp: Int, val defense: Int, val attack: Int)
+data class Pokemon(val name: String, val maxHp: Int, val defense: Int, val attack: Int, var currentHp: Int)
