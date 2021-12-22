7063caf0e253e3dc405b89094d8ab256b48ac77e
adrian130695 <adrian_alejandro95@hotmail.com>
2021-12-22 15:31:05 -0600

    added enemy turn and clean view model

--- pokimanapp/app/src/main/java/com/aa95/pokiman_app/viewmodel/MainViewModel.kt
+++ pokimanapp/app/src/main/java/com/aa95/pokiman_app/viewmodel/MainViewModel.kt
@@ -16,29 +16,41 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         isMyTurn.value = true
     }
 
-    private fun getPokemon(){
+    private fun getPokemon() {
         myPokemon.value = Pokemon("Pikachu", 100, 10, 10, 100)
         enemyPokemon.value = Pokemon("Raichu", 100, 10, 10, 100)
     }
 
-    fun attackPokemon(attacked: Boolean, damage: Int){
-        if(attacked){
-            myPokemon.value.apply {
-                this?.let {
-                    this.currentHp = if((this.currentHp - damage) <= 0) 0 else this.currentHp - damage
-                    myPokemon.value = this
-                    isMyTurn.value = true
-                }
+    fun attackPokemon(attacked: Boolean, damage: Int) {
+        if (attacked) {
+            myPokemon.value?.let {
+                it.currentHp =
+                    if ((it.currentHp - damage) <= 0) 0 else it.currentHp - damage
+                myPokemon.value = it
+                isMyTurn.value = true
             }
-        }else{
-            enemyPokemon.value.apply {
-                this?.let {
-                    this.currentHp = if ((this.currentHp - damage) <= 0) 0 else this.currentHp - damage
-                    enemyPokemon.value = this
-                    isMyTurn.value = false
-                }
+        } else {
+            enemyPokemon.value?.let {
+                it.currentHp = if ((it.currentHp - damage) <= 0) 0 else it.currentHp - damage
+                enemyPokemon.value = it
+                endTurn()
             }
+        }
+    }
+
+    private fun enemyTurn() {
+
+        enemyPokemon.value?.let {
+            attackPokemon(true, it.attack)
         }
 
     }
+
+    private fun endTurn(){
+
+        isMyTurn.value = false
+        enemyTurn()
+
+    }
+
 }
